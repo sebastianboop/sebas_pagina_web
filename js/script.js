10cb1ec7b2356ffcc02b752e97a74a6a20e97207
Sebas_Rendon <srendon8501@gmail.com>
2021-09-07 06:42:30 -0500

    agregando codigo js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(main);
+var contador = 0;
+function main(){
+    $('.menu-bar').click( function(){
+        if (contador == 0) {
+            $('nav').animate({
+                left: '0'
+            });
+            contador = 1;
+        }
+        else {
+            $('nav').animate({
+                left: '-100%'
+            });
+        }
+    });
+}
