8a043fedc2ac7c9588e625eb6f4901a46861772d
sebastianboop <srendon8501@gmail.com>
2021-12-09 11:47:21 -0500

    switch

--- JavaScript/js/21.swich.js
+++ JavaScript/js/21.swich.js
@@ -1,7 +1,15 @@
 "use strict"
 
 var voto= "Luisa";
-var candidatos= ["luisa","Jesus","Hamir", "nulo","blanco"];
-switch(voto){
-    case"Jesus"
+var candidatos= ["Luisa","Jesus","Hamir", "nulo","blanco"];
+var votos=[0,0,0,0,0,0];
+for(let i=0;i<candidatos.length; i++){
+    if(voto===candidatos[i]){
+        votos[i]++;
+    }
+}
+for(let i=0;i<candidatos.length; i++){
+    console.log("===RESULTADOS===")
+    console.log(candidatos[i]+" : "+ votos[i]);
+
 }
