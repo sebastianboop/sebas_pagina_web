91cd1ecfb32be7b7bd39d03370a15f2873dce515
Yoan <srendon8501@gmail.com>
2021-12-14 06:03:47 -0500

    calculadora

---
+++ JavaScript/js/calculadora.js
@@ -0,0 +1,22 @@
+
+//variables numericas
+
+var numero1 = 2;
+var numero2 = 100; 
+//crear funcion para suma
+
+function mostrarSuma(){
+    alert(numero1 +numero2);
+};
+function mostrarResta(){
+    alert(numero1-numero2);
+};
+function mostrarDivision(){
+    alert(numero1/numero2);
+};
+function mostrarMultiplicacion(){
+    alert(numero1*numero2);
+};
+function mostrarValores(){
+    alert("el primer numero es: "+ numero1 + " y el segundo es: " +numero2);
+};
