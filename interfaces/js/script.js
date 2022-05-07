b82cbb332c1ae08d52b63b650b56dc4298ef6b7b
Sebas_Rendon <srendon8501@gmail.com>
2022-05-07 18:42:49 -0500

    agregando interfaz de formulario con validacion

--- interfaces/js/script.js
+++ interfaces/js/script.js
@@ -1,1 +1,32 @@
 //alert("hello world");
+
+
+
+function validarFormulario(cod,descripcion,valor,stock){
+    var valor=  document.getElementById(valor); 
+    var cod = document.getElementById(cod);
+    var descripcion=document.getElementById(descripcion);
+    var stock= document.getElementById(stock);
+    if(cod.value === ""){
+        alert("Ingrese el codigo");
+    }
+    if(descripcion.value === ""){
+    alert("ingrese una descripcion"); 
+
+    }
+
+    if (valor.value === ""){
+        alert("por favor ingrese un valor");
+    }
+
+    if ( stock.value===""){
+        alert("por favor registre el stock del producto")
+    }
+    
+    
+
+    
+}
+
+
+
