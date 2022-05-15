cb1d67857ec0b66812e010c870cb58ad177da734
Sebas_Rendon <srendon8501@gmail.com>
2022-05-14 20:57:31 -0500

    haciendo un netflix

---
+++ interfaces/js/cartelera_cine.js
@@ -0,0 +1,35 @@
+function volver(){
+    document.getElementById("imgprincipal").src= "img/netflix.jpg";
+}
+
+function intercambiaracuaman(){
+    document.getElementById("imgprincipal").src= "img/acuaman.jpg";
+    
+}
+function intercambiardoc(){
+    document.getElementById("imgprincipal").src= "img/doc.jpg";
+}
+function intercambiarpadrino(){
+    document.getElementById("imgprincipal").src= "img/el_padrino_1920_1080.jpg";
+}
+function intercambiarcmon(){
+    document.getElementById("imgprincipal").src= "img/cmon2.jpg";
+}
+function intercambiarciudad(){
+    document.getElementById("imgprincipal").src= "img/1366_2000.jpg";
+}
+function intercambiarpesodeltalento(){
+    document.getElementById("imgprincipal").src= "img/pesotalento.jpg";
+}
+function intercambiarlapeorpersona(){
+    document.getElementById("imgprincipal").src= "img/peorpersona.jpg";
+}
+function intercambiarllamasdevenganza(){
+    document.getElementById("imgprincipal").src= "img/llamasdevenganza.jpg";
+}
+function intercambiarolga(){
+    document.getElementById("imgprincipal").src= "img/olga.jpg";
+}
+function intercambiarsonic(){
+    document.getElementById("imgprincipal").src= "img/sonic2.jpg";
+}
