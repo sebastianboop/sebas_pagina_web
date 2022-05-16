89af18564b1ab16b3648cfebb579422d7938eae4
sebastianboop <srendon8501@gmail.com>
2022-05-16 11:51:02 -0500

    adelantando el carrito de compra

---
+++ interfaces/js/carritocompras.js
@@ -0,0 +1,25 @@
+"use strict"
+
+function pruebaSuma(){
+    let cantidad = document.getElementById("cantidad").innerHTML;
+    if(cantidad> 0 && cantidad<10){
+        
+    cantidad = Number(cantidad)+1;
+    document.getElementById("cantidad").innerHTML = cantidad;
+
+    }else{
+        alert("solo puede comprar entre 1 y 10 libros");
+    }
+}
+
+function pruebaResta(){
+    let cantidad = document.getElementById("cantidad").innerHTML;
+    if(cantidad>1 && cantidad<=10){
+
+    cantidad = Number(cantidad)-1;
+    document.getElementById("cantidad").innerHTML = cantidad;
+}else{
+    alert("solo puede comprar entre 1 y 10 libros");
+}
+
+}
