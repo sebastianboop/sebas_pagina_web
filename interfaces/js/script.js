f77f7a0ad9fead34ec3e9012f2ef680266871f37
sebastianboop <srendon8501@gmail.com>
2022-05-16 10:49:50 -0500

    corrigirndo la validacion de el formulario

--- interfaces/js/script.js
+++ interfaces/js/script.js
@@ -1,7 +1,54 @@
-//alert("hello world");
+
+
+"use strict"
+
+function guardarInfo() {
+    let cod = document.getElementById("cod").value;
+    if (cod == "") {
+        alert("Por favor, ingrese el codigo de productos");
+        return false;
+    }
+    if (document.getElementById("descripcion").value == "") {
+        alert("Por favor, ingrese una descripcion");
+        return false;
+    }
+    if (document.getElementById("valor").value == "") {
+        alert("Por favor, ingrese un valor");
+        return false;
+    }
+    if (document.getElementById("stock").value == "") {
+        alert("Por favor, ingrese un stock");
+        return false;
+    }
+    alert("Información guardada con éxito");
+    limpiarFormulario();
+    return true;
+    
+}
+function limpiarFormulario() {
+    document.getElementById("cod").value = "";
+    document.getElementById("descripcion").value = "";
+    document.getElementById("valor").value = "";
+    document.getElementById("stock").value = "";
+}
 
 
 
+/*   
+function guardarinfo(){
+    if(validarFormulario()==true)
+    alert("hello")
+}
+function validarFormulario(){
+    let lvalor=  document.getElementById("valor").value; 
+    if(lvalor == ""){
+        alert("porfavor ingrese el valor");
+       
+
+    }let lcod = document.getElementById(cod);
+    let ldescripcion=document.getElementById(descripcion);
+    let lstock= document.getElementById(stock); 
+}
 function validarFormulario(cod,descripcion,valor,stock){
     var valor=  document.getElementById(valor); 
     var cod = document.getElementById(cod);
@@ -27,6 +74,6 @@ function validarFormulario(cod,descripcion,valor,stock){
 
     
 }
-
+*/
 
 
