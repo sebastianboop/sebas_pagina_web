4abfba1bf589ef5620580e92857b19bee0d182d6
Sebas_Rendon <srendon8501@gmail.com>
2022-05-22 17:44:58 -0500

    carrito de compras terminado

--- interfaces/js/scriptCarritoCompras.js
+++ interfaces/js/scriptCarritoCompras.js
@@ -1,7 +1,60 @@
-let valorUnitario = 27.11;
+//declaro la variable que del costo por unidad de mi producto
+let valorUnidad = 27.11;
+//llamo la funcion que me dice informacion de mis productos
+numeroProductos();
+//llamo otra funcion
+calcularSubtotal();
+//
 valorIndividual();
+//creo la funcion para determinar mi numero de productos
+function numeroProductos(){
+    let numeroProductos = document.getElementById("cantidadProductos").innerHTML;
+    document.getElementById("cantidad").innerHTML = numeroProductos;
+    if(numeroProductos >= 2 || numeroProductos == 0){
+        document.getElementById("cantidad").innerHTML= numeroProductos.toString()+ " productos";
+    }
+    else{
+        document.getElementById("cantidad").innerHTML= numeroProductos.toString()+ " producto";
+    }
+}
+
+function sumar(){
+    let cantidad=document.getElementById("cantidadProductos").innerHTML;
+    if(cantidad<=9){
+        cantidad=Number(cantidad)+1;
+        document.getElementById("cantidadProductos").innerHTML=cantidad;
+    }else{
+        alert(" no puedes comprar mas de 10 productos en este pedido");
+    }
+    calcularSubtotal();
+    numeroProductos();
+}
+function restar(){
+    let cantidad=document.getElementById("cantidadProductos").innerHTML;
+    if(cantidad>1){
+        cantidad=Number(cantidad) -1;
+        document.getElementById("cantidadProductos").innerHTML=cantidad;
+    }
+    else{
+        alert("no puedes comprar menos de un producto");
+    } 
+    calcularSubtotal();
+    numeroProductos();
+}
+function calcularSubtotal() {
+    let cantidad= document.getElementById("cantidadProductos").innerHTML;
+    let subtotal = 27.11 * Number(cantidad);
+    let subtotalFormat = new Intl.NumberFormat('es-ES').format(subtotal);
+    document.getElementById("valorSubtotal").innerHTML = subtotalFormat;
+}
+function valorIndividual() {
+    let valorUnitarioFormat = new Intl.NumberFormat('es-ES').format(valorUnidad);
+    document.getElementById("valor-unitario").innerHTML = valorUnitarioFormat,toString()+ " USD";
+}
+/*valorIndividual();
 calcularSubtotal();
 numeroProductosInfo();
+
 
 function numeroProductosInfo() {
     let numeroActual = document.getElementById("cantidadProductos").innerHTML;
@@ -16,7 +69,7 @@ function numeroProductosInfo() {
 }
 function calcularSubtotal() {
     let cantidadElementos = document.getElementById("cantidadProductos").innerHTML;
-    let subtotal = valorUnitario * Number(cantidadElementos);
+    let subtotal = 27.11 * Number(cantidadElementos);
     let subtotalFormat = new Intl.NumberFormat('es-ES').format(subtotal);
     document.getElementById("valorSubtotal").innerHTML = subtotalFormat;
 }
@@ -47,3 +100,4 @@ function valorIndividual() {
     let valorUnitarioFormat = new Intl.NumberFormat('es-ES').format(valorUnitario);
     document.getElementById("valor-unitario").innerHTML = valorUnitarioFormat;
 }
+*/
