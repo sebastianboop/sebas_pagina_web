d01fb5d9a608f2256eadd995c0d0925ffb590cd8
Sebas_Rendon <srendon8501@gmail.com>
2022-06-04 16:40:24 -0500

    buscador de mercadolibre

--- POO/js/mercadoLibre.js
+++ POO/js/mercadoLibre.js
@@ -12,8 +12,9 @@ class automovil{
     }
 }
 
-let car1= new automovil("mazda","prado","txl","2020","298.000.000","82","Bogota","img/toyotaPrado.png");
-
+let car1= new automovil("Mazda","prado","txl","2020","298000000","82","Bogota","img/image.jpeg");
+let car2= new automovil("Chevrolet","Onix","turbo","2020","62800000","90","Bogota","img/chevrolet-onix.jpeg");
+let car3= new automovil("Suzuki","jimmy","lite","2020","62500000","130","Pitalito","img/suzuki.jpeg");
 
 function Busqueda(car){
     let contenedorPrincipal=document.getElementById("contenidoPrincipal");
@@ -23,7 +24,7 @@ function Busqueda(car){
 
     let img= document.createElement("img");
     img.setAttribute("src", car.imagen);
-    img.setAttribute("src","img");
+    img.setAttribute("class","img");
     contenedorPrincipal.appendChild(img);
 
     let valorcar= document.createElement("label");
@@ -45,13 +46,20 @@ function Busqueda(car){
     let yearcar=document.createElement("label");
     let textyear=document.createTextNode(car.year);
     yearcar.appendChild(textyear);
-
     yearcar.setAttribute("class","year");
+    contenedorPrincipal.appendChild(yearcar);
 
     let kilometrajecar=document.createElement("label");
     let textkilometraje=document.createTextNode(car.kilometraje+" kilometros");
     kilometrajecar.appendChild(textkilometraje);
     kilometrajecar.setAttribute("class","kilometraje");
+    contenedorPrincipal.appendChild(kilometrajecar);
+
+    let city=document.createElement("label");
+    let textcity=document.createTextNode(car.ciudad);
+    city.appendChild(textcity);
+    city.setAttribute("class","ciudad");
+    contenedorPrincipal.appendChild(city);
 
 }
 
@@ -60,8 +68,15 @@ window.addEventListener("keydown",function(event){
     if(event.key=="Enter"){
         console.log(Busq);
         cleanwindow();
-        if(Busq=="hello"){
+        if(Busq=="mazda"){
             Busqueda(car1);           
+        }
+        else if(Busq=="chevrolet"){
+            Busqueda(car2);
+        }
+        
+        else if(Busq=="suzuki"){
+            Busqueda(car3);
         }
         else{
             alert("sin coincidencias");
